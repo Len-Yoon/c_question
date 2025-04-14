3e6fa4901bd843411690df645839da5ae8953945
AIWS-203-112 <mabest123@naver.com>
2025-04-14 09:44:45 +0900

    소수 판별 문제

--- c_question/question1_250414.cpp
+++ c_question/question1_250414.cpp
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-    
+    int a;
+    int count = 0;
+
+    printf("소수인지 알고싶은 숫자를 적으세요: \n");
+    scanf("%d", &a);
+
+    for (int i = 1; i <= a; i++) {
+        if (a % i == 0) {
+            count++;
+        }
+    }
+
+    if (count > 2) {
+        printf("소수가 아닙니다.\n");
+    } else {
+        printf("소수가 맞습니다.\n");
+    }
 }
